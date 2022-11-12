c2b5c11c05a92058357dac8458188ddec0179d7b
Manav Misra <manavm@visionwebsoft.com>
2022-11-12 00:20:43 +0000

    WIP api service

---
+++ src/services/api.service.js
@@ -0,0 +1,18 @@
+/**
+ * HINTS:
+ * 1. For the repo issues, be sure to return just the response, and not the actual data.
+ * 2. For the repo issues, you'll need to pass in the repo
+ * 3. The URL for getting repos by topics looks like this:
+ *  'https://api.github.com/search/repositories?q=javascript+is:featured&sort=help-wanted-issues'
+ * 4. The URL for getting issues looks like this:
+ * 'https://api.github.com/repos/facebook/react/issues?direction=asc'
+ * Note that 'facebook/react' is the repo name (you can just use one parameter for this)
+ * 5. The URL for getting a user's repos looks like this:
+ * 'https://api.github.com/users/USERNAME/repos'
+ */
+
+const BASE_URL = "http://api.github.com";
+
+export default {
+  // TODO: Add methods for getting repos by topic, issues, and user repos
+};
