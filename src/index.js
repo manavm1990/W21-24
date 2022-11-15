afc02c412ae97eabdd0e48cb1bffbd60fbcb0fab
Manav Misra <manavm@visionwebsoft.com>
2022-11-14 20:03:48 -0600

    Log the data from the apiService
    Uses the username from the form.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@
  * If it's a 'default' export, no need to use curly braces.
  */
 
+import apiService from "./services/api.service";
+
 document.querySelectorAll("button").forEach((button) => {
   button.classList.add("button");
 });
 
-document.querySelector("form").addEventListener("submit", (event) => {
+document.querySelector("form").addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const username = event.target.username.value;
+
+  const data = await apiService.getReposByUsername(username);
+
+  console.log(data);
 });
